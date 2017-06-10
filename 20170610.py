6f08afaed6e395f200dd616b0e57c4ab3f9048d7
seonghun kang <kang9693@naver.com>
2017-06-10 14:17:11 +0900

    20170610 2차

---
+++ 20170610.py
@@ -0,0 +1,23 @@
+print("강성훈 입니다.!!")
+
+cd c:\workspace\py_5m\
+cd d:\workspace\py_5m\pybasic5 
+
+pybasic5 # <== 프로젝트  
+git pull 
+
+
+#http://www.github.com/kang9693/pybasic5
+#http://www.github.com/kang9693/< kang9693 계정 소유하고 있미
+#kang9693 > 각자 이름 git 프로젝트를 만들어 보세요.
+#외국계  취업시 it회사는 github 또는 오픈소스 프로젝트에 기여한
+#자료를 요청 하기도 한다고 합니다. 
+#MS evtx 파일 에대해서 구체적 공개 하지 않기 때문에 
+# 취업, 포렌식 (침해사고)
+# 디지털포렌식, 사이버포렌식, cyber crime forensic 
+# cyber forensic 
+# evtx win7  분석대상이 win7으로 한정 
+# xml  / 경력 
+
+## 악성 분석 동적분석, 정적분석, 동적분석,,, 정적 리버성 수행 .. 
+#  동적분석 < == tools 사용법  
